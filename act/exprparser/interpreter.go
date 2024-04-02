2ca53dd6605091ee4b022842296701ae4d55aa93
TheFox0x7 <thefox0x7@gmail.com>
2024-04-02 18:23:26 +0000

    [FORGEJO] add forge alias for github (#37)
    I left gitea for backwards compatibility
    
    Reviewed-on: https://code.forgejo.org/forgejo/act/pulls/37
    Reviewed-by: earl-warren <earl-warren@noreply.code.forgejo.org>
    Co-authored-by: TheFox0x7 <thefox0x7@gmail.com>
    Co-committed-by: TheFox0x7 <thefox0x7@gmail.com>

--- act/exprparser/interpreter.go
+++ act/exprparser/interpreter.go
@@ -157,6 +157,8 @@ func (impl *interperterImpl) evaluateVariable(variableNode *actionlint.VariableN
 		return impl.env.Github, nil
 	case "gitea": // compatible with Gitea
 		return impl.env.Github, nil
+	case "forge":
+		return impl.env.Github, nil
 	case "env":
 		return impl.env.Env, nil
 	case "job":
