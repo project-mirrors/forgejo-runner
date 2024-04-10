5660e21fb8cf58fa6c42b02d97afd26410ef312f
TheFox0x7 <thefox0x7@gmail.com>
2024-04-10 22:39:55 +0200

    added simple test to label update

---
+++ internal/app/run/runner_test.go
@@ -0,0 +1,38 @@
+package run
+
+import (
+	"context"
+	"testing"
+
+	"gitea.com/gitea/act_runner/internal/pkg/labels"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLabelUpdate(t *testing.T) {
+	ctx := context.Background()
+	ls := labels.Labels{}
+
+	initialLabel, err := labels.Parse("testlabel:docker://alpine")
+	assert.Nil(t, err)
+	ls = append(ls, initialLabel)
+
+	newLs := labels.Labels{}
+
+	newLabel, err := labels.Parse("next label:host")
+	assert.Nil(t, err)
+	newLs = append(newLs, initialLabel)
+	newLs = append(newLs, newLabel)
+
+	runner := Runner{
+		labels: ls,
+	}
+
+	assert.Contains(t, runner.labels, initialLabel)
+	assert.NotContains(t, runner.labels, newLabel)
+
+	runner.Update(ctx, newLs)
+
+	assert.Contains(t, runner.labels, initialLabel)
+	assert.Contains(t, runner.labels, newLabel)
+
+}
