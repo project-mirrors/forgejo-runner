project-mirrors/forgejo-runner

commit 5a351d33df5a3ffc3ac8523593a440e4843c341b

refactor: ignore already closed error (#1285)
* refactor: ignore already closed error

This hides the `file already cloesd` error as it is
distracting in the output and does not provide any value.

* refactor: use go errors

Co-authored-by: Casey Lee <caseypl@amazon.com>

act/container/docker_run.go
@@ -5,6 +5,7 @@ import (
 	"bufio"
 	"bytes"
 	"context"
+	"errors"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -696,7 +697,7 @@ func (cr *containerReference) copyDir(dstPath string, srcPath string, useGitIgno
 		defer func(tarFile *os.File) {
 			name := tarFile.Name()
 			err := tarFile.Close()
-			if err != nil {
+			if !errors.Is(err, os.ErrClosed) {
 				logger.Error(err)
 			}
 			err = os.Remove(name)

commit 324188e23938bdaae532923402e70409d429d58a

fix: Do not fail on nil error
Commit 5a351d33df5a3ffc3ac8523593a440e4843c341b has introduced a
regression due to which copyDir() would fail after closing the archive
successfully

Signed-off-by: Vitaliy Potyarkin <v.potyarkin@yadro.com>

act/container/docker_run.go
@@ -753,7 +753,7 @@ func (cr *containerReference) copyDir(dstPath string, srcPath string, useGitIgno
 		defer func(tarFile *os.File) {
 			name := tarFile.Name()
 			err := tarFile.Close()
-			if !errors.Is(err, os.ErrClosed) {
+			if err != nil && !errors.Is(err, os.ErrClosed) {
 				logger.Error(err)
 			}
 			err = os.Remove(name)