project-mirrors/forgejo-runner

commit 4880b091a2dbf0cffaf2064227094eea11e0619b

[RDNF #11] fix: skipped jobs have no result & fix: favor command-line over files & chore: fix some comments (#143)
https://github.com/nektos/act/pull/2274
https://github.com/nektos/act/pull/2276
https://github.com/nektos/act/pull/2279
Co-authored-by: lvyaoting <166296299+lvyaoting@users.noreply.github.com>
Co-authored-by: Josh Soref <2119212+jsoref@users.noreply.github.com>
Co-authored-by: ChristopherHX <christopher.homberger@web.de>
Reviewed-on: https://code.forgejo.org/forgejo/act/pulls/143
Reviewed-by: earl-warren <earl-warren@noreply.code.forgejo.org>
Co-authored-by: achyrva <achyrva@noreply.code.forgejo.org>
Co-committed-by: achyrva <achyrva@noreply.code.forgejo.org>

act/runner/run_context.go
@@ -914,7 +914,8 @@ func (rc *RunContext) isEnabled(ctx context.Context) (bool, error) {
 	}
 
 	if !runJob {
-		l.WithField("jobResult", "skipped").Infof("Skipping job '%s' due to '%s'", job.Name, job.If.Value)
+		rc.result("skipped")
+		l.WithField("jobResult", "skipped").Debugf("Skipping job '%s' due to '%s'", job.Name, job.If.Value)
 		return false, nil
 	}
 

act/runner/hashfiles/index.js
@@ -853,7 +853,7 @@ class Summary {
     /**
      * If the summary buffer is empty
      *
-     * @returns {boolen} true if the buffer is empty
+     * @returns {boolean} true if the buffer is empty
      */
     isEmptyBuffer() {
         return this._buffer.length === 0;
@@ -1840,7 +1840,7 @@ class Pattern {
             // Normalize slashes
             itemPath = pathHelper.normalizeSeparators(itemPath);
             // Append a trailing slash. Otherwise Minimatch will not match the directory immediately
-            // preceeding the globstar. For example, given the pattern `/foo/**`, Minimatch returns
+            // preceding the globstar. For example, given the pattern `/foo/**`, Minimatch returns
             // false for `/foo` but returns true for `/foo/`. Append a trailing slash to handle that quirk.
             if (!itemPath.endsWith(path.sep)) {
                 // Note, this is safe because the constructor ensures the pattern has an absolute root.

act/runner/step_test.go
@@ -314,7 +314,7 @@ func TestIsContinueOnError(t *testing.T) {
 	assertObject.False(continueOnError)
 	assertObject.Nil(err)
 
-	// explcit true
+	// explicit true
 	step = createTestStep(t, "continue-on-error: true")
 	continueOnError, err = isContinueOnError(context.Background(), step.getStepModel().RawContinueOnError, step, stepStageMain)
 	assertObject.True(continueOnError)

cmd/root.go
@@ -333,7 +333,9 @@ func readEnvs(path string, envs map[string]string) bool {
 			log.Fatalf("Error loading from %s: %v", path, err)
 		}
 		for k, v := range env {
-			envs[k] = v
+			if _, ok := envs[k]; !ok {
+				envs[k] = v
+			}
 		}
 		return true
 	}

commit 7eb547faa5acbe09f65fefd522a894ccb5aebd9c

fix: do not fail the job when `if: false` (#172)
- log job result as info not as debug
- add test

---

v6.4.0 regression introduced in 4880b091a2dbf0cffaf2064227094eea11e0619b

It did not fail a test because the [original fix](https://code.forgejo.org/forgejo/act/pulls/67/files)  has tests only for the case where a step is skipped, not when a job is skipped.

Closes forgejo/runner#660

Reviewed-on: https://code.forgejo.org/forgejo/act/pulls/172
Reviewed-by: Michael Kriese <michael.kriese@gmx.de>
Co-authored-by: Earl Warren <contact@earl-warren.org>
Co-committed-by: Earl Warren <contact@earl-warren.org>

act/runner/run_context.go
@@ -914,7 +914,7 @@ func (rc *RunContext) isEnabled(ctx context.Context) (bool, error) {
 
 	if !runJob {
 		rc.result("skipped")
-		l.WithField("jobResult", "skipped").Debugf("Skipping job '%s' due to '%s'", job.Name, job.If.Value)
+		l.WithField("jobResult", "skipped").Infof("Skipping job '%s' due to '%s'", job.Name, job.If.Value)
 		return false, nil
 	}
 

act/runner/runner_test.go
@@ -520,7 +520,8 @@ func TestRunDifferentArchitecture(t *testing.T) {
 }
 
 type runSkippedHook struct {
-	found bool
+	resultKey string
+	found     bool
 }
 
 func (h *runSkippedHook) Levels() []log.Level {
@@ -528,8 +529,8 @@ func (h *runSkippedHook) Levels() []log.Level {
 }
 
 func (h *runSkippedHook) Fire(entry *log.Entry) error {
-	if v, ok := entry.Data["stepResult"]; ok {
-		h.found = (v == model.StepStatusSkipped)
+	if result, ok := entry.Data[h.resultKey]; ok {
+		h.found = (fmt.Sprintf("%s", result) == "skipped")
 	}
 	return nil
 }
@@ -539,21 +540,25 @@ func TestRunSkipped(t *testing.T) {
 		t.Skip("skipping integration test")
 	}
 
-	tjfi := TestJobFileInfo{
-		workdir:      workdir,
-		workflowPath: "skip",
-		eventName:    "push",
-		errorMessage: "",
-		platforms:    platforms,
+	for _, what := range []string{"step", "job"} {
+		t.Run(what, func(t *testing.T) {
+			tjfi := TestJobFileInfo{
+				workdir:      workdir,
+				workflowPath: "skip" + what,
+				eventName:    "push",
+				errorMessage: "",
+				platforms:    platforms,
+			}
+
+			h := &runSkippedHook{resultKey: what + "Result"}
+			ctx := common.WithLoggerHook(context.Background(), h)
+
+			jobLoggerLevel := log.InfoLevel
+			tjfi.runTest(ctx, t, &Config{ContainerArchitecture: "linux/arm64", JobLoggerLevel: &jobLoggerLevel})
+
+			assert.True(t, h.found)
+		})
 	}
-
-	h := &runSkippedHook{}
-	ctx := common.WithLoggerHook(context.Background(), h)
-
-	jobLoggerLevel := log.InfoLevel
-	tjfi.runTest(ctx, t, &Config{ContainerArchitecture: "linux/arm64", JobLoggerLevel: &jobLoggerLevel})
-
-	assert.True(t, h.found)
 }
 
 type maskJobLoggerFactory struct {

act/runner/testdata/skipjob/skipjob.yml
@@ -1,0 +1,11 @@
+name: skipjob
+on: push
+
+jobs:
+  checkjob:
+    runs-on: ubuntu-latest
+
+    if: false
+
+    steps:
+      - run: echo nothing

act/runner/testdata/skipstep/skipstep.yml
@@ -1,8 +1,8 @@
-name: skip
+name: skipstep
 on: push
 
 jobs:
-  check:
+  checkstep:
     runs-on: ubuntu-latest
     steps:
       - if: false