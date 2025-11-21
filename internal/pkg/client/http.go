63ed37b55046aa6549d80f50e3b8b89ebd7a5f68
Mathieu Fenniak <mathieu@fenniak.net>
2025-11-21 13:45:41 +0000

    fix: ensure http.Client always has a timeout for Forgejo access (#1171)
    Prevent possible frozen process due to irregular network activity when uploading logs, re: #1166.
    
    <!--start release-notes-assistant-->
    <!--URL:https://code.forgejo.org/forgejo/runner-->
    - bug fixes
      - [PR](https://code.forgejo.org/forgejo/runner/pulls/1171): <!--number 1171 --><!--line 0 --><!--description Zml4OiBlbnN1cmUgaHR0cC5DbGllbnQgYWx3YXlzIGhhcyBhIHRpbWVvdXQgZm9yIEZvcmdlam8gYWNjZXNz-->fix: ensure http.Client always has a timeout for Forgejo access<!--description-->
    <!--end release-notes-assistant-->
    
    Reviewed-on: https://code.forgejo.org/forgejo/runner/pulls/1171
    Reviewed-by: limiting-factor <limiting-factor@noreply.code.forgejo.org>
    Co-authored-by: Mathieu Fenniak <mathieu@fenniak.net>
    Co-committed-by: Mathieu Fenniak <mathieu@fenniak.net>

--- internal/pkg/client/http.go
+++ internal/pkg/client/http.go
@@ -7,25 +7,48 @@ import (
 	"context"
 	"crypto/tls"
 	"net/http"
+	"os"
+	"strconv"
 	"strings"
+	"time"
 
 	"code.forgejo.org/forgejo/actions-proto/ping/v1/pingv1connect"
 	"code.forgejo.org/forgejo/actions-proto/runner/v1/runnerv1connect"
 	"connectrpc.com/connect"
+	log "github.com/sirupsen/logrus"
 )
 
 func getHTTPClient(endpoint string, insecure bool) *http.Client {
+	transport := &http.Transport{
+		Proxy: http.ProxyFromEnvironment,
+	}
+
 	if strings.HasPrefix(endpoint, "https://") && insecure {
-		return &http.Client{
-			Transport: &http.Transport{
-				Proxy: http.ProxyFromEnvironment,
-				TLSClientConfig: &tls.Config{
-					InsecureSkipVerify: true,
-				},
-			},
+		transport.TLSClientConfig = &tls.Config{
+			InsecureSkipVerify: true,
 		}
 	}
-	return http.DefaultClient
+
+	// Typically all accesses to the Forgejo server will occur in a context with a timeout, like when running a step or
+	// an action, but occasionally some actions like closing a `Reporter` will occur in `context.Background` to ensure
+	// that transmission of the final logs of an action can progress even when the job is timed out.  In those cases,
+	// it's important to have an HTTP-level timeout (which `http.DefaultClient` does not have) so that the processes
+	// don't get frozen in the event of irregular network activity.  Overriddable with FORGEJO_CLIENT_TIMEOUT.
+	timeout := 60 * time.Second
+	env := os.Getenv("FORGEJO_CLIENT_TIMEOUT")
+	if env != "" {
+		timeoutSeconds, err := strconv.ParseInt(env, 10, 64)
+		if err != nil {
+			log.Errorf("Failed to parse FORGEJO_CLIENT_TIMEOUT: %v", err)
+		} else {
+			timeout = time.Duration(timeoutSeconds) * time.Second
+		}
+	}
+
+	return &http.Client{
+		Transport: transport,
+		Timeout:   timeout,
+	}
 }
 
 // New returns a new runner client.
